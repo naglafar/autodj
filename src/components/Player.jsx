2a9022561e593cf602dab88ade51971d5dc57fe7
Tom Parkinson <thomas.r.parkinson@gmail.com>
2016-01-21 15:23:46 +0000

    fading in and fading out of tracks

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,34 +9,136 @@ const Col = require('./Column.jsx'),
 const flux = require('../flux'),
   storeNames = require('../constants/storeNames');
 
-const sources = {
-  MAIN_SOURCE: 'main.source',
-  NEW_SOURCE: 'next.source'
-};
+const fadeTimeSeconds = 2;
 
 const Player = React.createClass({
 
   getInitialState: () => {
     return {
       playing: false,
+      trackLoaded: false,
       context: new AudioContext()
     };
   },
 
   play: function () {
     let tracks = flux.store(storeNames.PLAY_LIST).getState().tracks,
-      currentTrack = tracks[0];
-    this.playTrack(currentTrack, sources.MAIN_SOURCE);
+      currentTrack = tracks[0],
+      that = this;
+
+    Promise.resolve()
+
+    .then(
+      () => {
+        console.log('starting playing');
+        that.setState({playing: true});
+      }
+    )
+
+    .then(() => {
+      return that.prepTrack(currentTrack);
+    })
+
+    .then(({source, gain}) => {
+      return that.fadeIn(source, gain, 0);
+    })
+
+    .then(({source, gain}) => {
+      return that.savePlayingTrack(source, gain);
+    });
   },
 
   pause: function () {
-    this.state[sources.MAIN_SOURCE].stop();
-    this.state[sources.NEW_SOURCE].stop();
-    this.setState({playing: false});
+    let that = this,
+      source = this.state.source,
+      gain = this.state.gain;
+
+    Promise.resolve()
+    .then(
+      () => {
+        return that.fadeOut(source, gain);
+      }
+    );
+  },
+
+  resume: function () {
+
+  },
+
+  savePlayingTrack: function (source, gain) {
+    const that = this;
+    return new Promise((resolve) => {
+      that.setState({
+        source: source,
+        gain: gain
+      });
+      console.debug('now playing');
+      resolve();
+    });
+  },
+
+  prepTrack: function (track) {
+    const that = this;
+    console.debug('preparing track', track);
+    return new Promise((resolve) => {
+      const context = that.state.context;
+      context.decodeAudioData(track.arrayBuffer, function (buffer) {
+        let source = context.createBufferSource();
+        let gainNode = context.createGain();
+        source.buffer = buffer;
+        source.connect(gainNode);
+        gainNode.connect(context.destination);
+        console.log('track prepared', track);
+        resolve({source: source, gain: gainNode});
+      });
+    });
+  },
+
+  fadeIn: function (source, gainNode) {
+    const that = this;
+    return new Promise((resolve) => {
+      console.debug('fading in');
+      const context = that.state.context,
+        currentTime = context.currentTime;
+
+      gainNode.gain.linearRampToValueAtTime(0, currentTime);
+      gainNode.gain.linearRampToValueAtTime(1, currentTime + fadeTimeSeconds);
+
+      source.start(0);
+
+      setTimeout(() => {
+        console.debug('faded track in');
+        resolve({source: source, gain: gainNode});
+      },
+      fadeTimeSeconds * 1000
+      );
+    });
+  },
+
+  fadeOut: function (source, gainNode) {
+
+    const context = this.state.context,
+      currentTime = context.currentTime;
+
+    return new Promise((resolve) => {
+      gainNode.gain.linearRampToValueAtTime(1, currentTime);
+      gainNode.gain.linearRampToValueAtTime(0, currentTime + fadeTimeSeconds);
+
+      setTimeout(() => {
+        source.stop();
+        resolve();
+      },
+        fadeTimeSeconds * 1000
+      );
+    });
+  },
+
+  moveTheQueueAlongOne: function () {
+
   },
 
   skip: function () {
-    let tracks = flux.store(storeNames.PLAY_LIST).getState().tracks,
+   /* let tracks = flux.store(storeNames.PLAY_LIST).getState().tracks,
       nextTrack = tracks[1];
 
     // fade in
@@ -48,7 +150,7 @@ const Player = React.createClass({
     this.state[sources.MAIN_SOURCE].stop();
     this.state[sources.MAIN_SOURCE] = this.state[sources.NEW_SOURCE];
 
-    delete this.state[sources.NEW_SOURCE];
+    delete this.state[sources.NEW_SOURCE];*/
   },
 
   playTrack: function (track, layer) {
